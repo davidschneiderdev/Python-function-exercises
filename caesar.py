452d121a812982c49d211cff014fafc099551b23
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-12 14:59:09 -0500

    Caesar cypher using functions

---
+++ caesar.py
@@ -0,0 +1,40 @@
+
+# convert a letter
+def convert_letter(letter, rotate_by=13):
+    # rotate_by is an optional argument.
+
+    # 1. Setup/configuration
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+
+    # 2. Work
+    position = alphabet.index(letter)
+    new_position = (position + rotate_by) % 26
+    new_letter = alphabet[new_position]
+
+    # 3. Result
+    return new_letter
+
+def convert_sentence(sentence):
+    new_list = []
+    for letter in list(sentence):
+        if letter == " ":
+            new_list.append(letter)
+        else:    
+            new_list.append(convert_letter(letter))
+    new_sentence = ''.join(new_list)
+    return new_sentence
+
+    # new_sentence = ""
+    # for letter in sentence:
+    #     if letter == " ":
+    #         new_sentence += letter
+    #     else:
+    #         new_sentence += convert_letter(letter)
+    # return new_sentence
+
+sentence = "lbh zhfg hayrnea jung lbh unir yrnearq"
+new_sentence = convert_sentence(sentence)
+print(new_sentence)
+
+
+
