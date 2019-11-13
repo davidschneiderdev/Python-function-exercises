ca952fd392b6fb02d42f4e1e743b2ee1be78cbef
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-13 14:04:06 -0500

    Updates to Find Longest String

--- medium_exercises/find_longest_str.py
+++ medium_exercises/find_longest_str.py
@@ -4,5 +4,10 @@ str_list = ["banana", "far", "apple", "orange", "pineapple"]
 def longest_str(str_list):
     return sorted(str_list, key=len)[-1]
 
-print(longest_str(str_list))
+longest_word = longest_str(str_list)
+print(longest_word)
+
+
+
+
 
